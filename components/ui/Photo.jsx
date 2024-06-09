609903d9bf2389c7726d177696c346f139a875a8
Taha <itxtaha09@gmail.com>
2024-06-09 16:26:22 +0500

    Portfolio

---
+++ components/ui/Photo.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+
+const Photo = () => {
+    return (
+        <div>Photo</div>
+    )
+}
+
+export default Photo
