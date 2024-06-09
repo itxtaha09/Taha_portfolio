5260c1629fa9719d83429010897cacdef4dfd17d
Taha <itxtaha09@gmail.com>
2024-06-09 09:36:42 +0500

    portfolio website

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { FiDownload} from "react-icons/fi";
+
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">homepage
+        <div className="flex flex:col xl:flex items-center justify-between xl:pt-8 xl:pb-24">
+          <div>text</div>
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
