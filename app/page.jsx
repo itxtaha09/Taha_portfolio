c86f13c6cdb5161a180c86d17e04fee3fe07dd60
Taha <itxtaha09@gmail.com>
2024-06-10 23:06:17 +0500

    changes done

--- app/page.jsx
+++ app/page.jsx
@@ -9,18 +9,17 @@ const Home = () => {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
-        <div className="flex flex:col xl:flex items-center justify-between xl:pt-8 xl:pb-24">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/*text*/}
-          {/* mashkook div */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
-            <h1 className="h1">
+            <h1 className="h1 mb-6">
               Hello Im <br /> <span className="text-accent">Muhammad Taha</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
               I excel at crafting elegant digital experiences and I am
               proficient in various programming languages and technologies.
-            </p>
+            </p>  
             {/*btn and socials*/}
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <Button
@@ -34,14 +33,14 @@ const Home = () => {
               <div className="mb-8 xl:mb-0">
                 <Social
                   containerStyles="flex gap-6"
-                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-center text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"  
                 />
               </div>
             </div>
           </div>
           {/*photo*/}
         </div>
-        <div>
+        <div className="order-1 xl:order-none mb-8 xl:mb-0">
           <Photo />
         </div>
       </div>
--- components/ui/Photo.jsx
+++ components/ui/Photo.jsx
@@ -6,7 +6,32 @@ import Image from "next/image";
 
 const Photo = () => {
     return (
-        <div>Photo</div>
+        <div className="w-full h-full relative">
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{
+                    opacity: 1,
+                    transition: { delay: 2, duration: 0.4, ease: 'easeIn' },
+                }}
+            >
+                {/*image */}
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{
+                        opacity: 1,
+                        transition: { delay: 2.4, duration: 0.4, ease: 'easeInOut' },
+                        }}
+                    className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten">
+                    <Image 
+                    src="/assets/photo1.png"
+                    priority quality={100} 
+                    fill 
+                    alt="" 
+                    className="object-contain" />
+                </motion.div>
+                    {/*circle */}
+            </motion.div>
+        </div>
     )
 }
 
